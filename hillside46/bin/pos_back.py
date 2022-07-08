c101239efb76f307cb58acf74ad15b8892bd9114
mmccoyd <mmccoyd@cs.berkley.edu>
2022-07-07 17:42:36 -0700

    KiBot: rotate bottom 180, collect files

--- hillside46/bin/pos_back.py
+++ hillside46/bin/pos_back.py
@@ -3,17 +3,18 @@
 import sys
 import csv
 
-FIELDS = ['Ref', 'Val', 'Package', 'PosX', 'PosY', 'Rot', 'Side']
+FIELDS = ['Designator', 'Val', 'Package', 'Mid X', 'Mid Y', 'Rotation', 'Layer']
 
 def main():
     reader = csv.DictReader(sys.stdin)
     writer = csv.DictWriter(sys.stdout, FIELDS, quoting=csv.QUOTE_MINIMAL)
     writer.writeheader()
     for row in reader:
-        assert row['Side'] == 'top'
-        row['Side'] = 'bottom'
-        #assert row['PosX'][0] != '-'
-        #row['PosX'] = '-' = row['PosX']
+        assert row['Layer'] == 'top'
+
+        row['Layer'] = 'bottom'
+        row['Rotation'] = (row['Rotation'] + 180) % 360
+
         writer.writerow(row)
 
 if __name__ == "__main__":
