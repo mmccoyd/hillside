04593a50d4ff6b25841be27c3e85e973d9688a5b
mmccoyd <mmccoyd@cs.berkley.edu>
2022-11-02 20:17:39 -0700

    Workflow: PCBA tweak origin and rotations
    - Pos file not use aux axis for JLC
    - Bottom rotation is negative of top
    - BAV70 bottom rotation gets extra 180

--- bin/fab_pos_back.py
+++ bin/fab_pos_back.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
 
-"""Create position file for PCBA on back of board, rotating each 180 degrees."""
+"""Create position file for PCBA on back of board, mostly negating rotation."""
 
 import sys
 import csv
 
 FIELDS = ['Designator', 'Val', 'Package', 'Mid X', 'Mid Y', 'Rotation', 'Layer']
+"""
+Negate rotation. If BAV70, also add 180 mod 360.
 
+The power switch SPDT and the BAV70 do not like the same method.
+Adding to the BAV70 is one way to solve that.
+"""
 def main():
     reader = csv.DictReader(sys.stdin)
     writer = csv.DictWriter(sys.stdout, FIELDS, quoting=csv.QUOTE_MINIMAL)
     writer.writeheader()
     for row in reader:
         row['Layer'] = 'bottom'
-        row['Rotation'] = f"{(float(row['Rotation']) + 180) % 360:10.6f}"
+        rot = float(row['Rotation']) * -1
+        if row['Val'] == 'BAV70':
+            rot = (rot + 180) % 360
+        row['Rotation'] = f"{rot:10.6f}"
 
         writer.writerow(row)
 
